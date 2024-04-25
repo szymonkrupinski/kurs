69cf1c2a6603e00eea0132ba096bff10d2eef294
szymonkrupinski <szymon@hub.pl>
2024-04-25 12:15:23 +0200

    402 Utworzenie API

---
+++ client/src/main/java/com/example/client/Controller.java
@@ -0,0 +1,17 @@
+package com.example.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @Value("${uid}")
+    private String uid;
+
+
+    @GetMapping("/uid")
+    public  String getUUId(){
+        return uid;
+    }
+}
